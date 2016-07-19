1c5937cc73901d461b56e90b65de4a9269f96d59
utras-jerry <swang111@LAMU1MRG4HTG944.uhc.com>
2016-07-19 16:03:02 -0400

    functional breadcrumb

--- js/content.js
+++ js/content.js
@@ -1,23 +1,23 @@
-$("#aboutBox").one('click', function(){
-	breadcrumbValue += " > About";
+$("#aboutBox").click(function(){
+	breadcrumbValue = "<span id='homeBC'>Home</span> > <span id='contentBC'>Table of Contents</span> > <span id='aboutBC'>About</span>";
 	$("#content").fadeOut(delay, 'swing');
 	setTimeout('$("#about").fadeIn(500, "swing")', delay);
 	setTimeout('$("#breadcrumb").html(breadcrumbValue)', delay);
 });
-$("#resumeBox").one('click', function(){
-	breadcrumbValue += " > Resume";
+$("#resumeBox").click(function(){
+	breadcrumbValue = "<span id='homeBC'>Home</span> > <span id='contentBC'>Table of Contents</span> > <span id='resumeBC'>Resume</span>";
 	$("#content").fadeOut(delay, 'swing');
 	setTimeout('$("#resume").fadeIn(500, "swing")', delay);
 	setTimeout('$("#breadcrumb").html(breadcrumbValue)', delay);
 });
-$("#projectsBox").one('click', function(){
-	breadcrumbValue += " > Projects";
+$("#projectsBox").click(function(){
+	breadcrumbValue = "<span id='homeBC'>Home</span> > <span id='contentBC'>Table of Contents</span> > <span id='projectsBC'>Projects</span>";
 	$("#content").fadeOut(delay, 'swing');
 	setTimeout('$("#projects").fadeIn(500, "swing")', delay);
 	setTimeout('$("#breadcrumb").html(breadcrumbValue)', delay);
 });
-$("#contactBox").one('click', function(){
-	breadcrumbValue += " > Contact";
+$("#contactBox").click(function(){
+	breadcrumbValue = "<span id='homeBC'>Home</span> > <span id='contentBC'>Table of Contents</span> > <span id='contactBC'>Contact</span>";
 	$("#content").fadeOut(delay, 'swing');
 	setTimeout('$("#contact").fadeIn(500, "swing")', delay);
 	setTimeout('$("#breadcrumb").html(breadcrumbValue)', delay);
