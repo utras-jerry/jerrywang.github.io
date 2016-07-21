85779076a2440522445b02ea19bd6b01fc46bd2f
utras-jerry <swang111@LAMU1MRG4HTG944.uhc.com>
2016-07-21 13:46:25 -0400

    debugging...

--- email.php
+++ email.php
@@ -10,4 +10,23 @@
     $fields{"message"} = "message";
     $body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){   $body .= sprintf("%20s: %s\n",$b,$_REQUEST[$a]); }
     $send = mail($to, $subject, $body, $headers);
+
+echo '<pre>';
+print_r($to);
+echo '</pre>';
+exit;
+
+
+<?php
+/**
+ * Send debug code to the Javascript console
+ */ 
+function debug_to_console($data) {
+    if(is_array($data) || is_object($data))
+    {
+        echo("<script>console.log('PHP: ".json_encode($data)."');</script>");
+    } else {
+        echo("<script>console.log('PHP: ".$data."');</script>");
+    }
+}
 ?>
